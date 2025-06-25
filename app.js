7b93db64e47f4afa25b580b1f14331e137a01599
Polina <poinkash28@gmail.com>
2025-06-25 21:08:46 +0300

    Test CD on branch

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const eventRoutes = require('./routes/events');
 require('dotenv').config();
+const Event = require('./models/schema');
+const client = require('prom-client'); 
+
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics(); 
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests received',
+});
+
+const eventsCountGauge = new client.Gauge({
+  name: 'total_events_in_db',
+  help: 'Current number of events in the database'
+});
+app.use((req, res, next) => {
+  httpRequestCounter.inc();
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+    try {
+    const count = await Event.countDocuments();
+    console.log('Total events in DB:', count);
+    eventsCountGauge.set(count); 
+
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+  } catch (err) {
+    res.status(500).end(err.toString());
+  }
+});
 
 
 app.use(express.json());
